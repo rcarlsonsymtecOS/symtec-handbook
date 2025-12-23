702eaadab2eb58d3b3230d712a8e8a51203cca20
rcarlsonsymtecOS <rcarlson@symtec.com>
2025-12-22 17:19:46 -0700

    Create index.js
    Add ask-handbook Azure Function

---
+++ api/ask-handbook/index.js
@@ -0,0 +1,11 @@
+module.exports = async function (context, req) {
+  context.res = {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: {
+      message: "SymTec Handbook Assistant is online."
+    }
+  };
+};
