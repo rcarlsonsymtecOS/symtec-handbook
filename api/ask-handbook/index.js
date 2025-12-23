7262689f221b3c956e8ab2479ef99ecf47aa38bb
rcarlsonsymtecOS <rcarlson@symtec.com>
2025-12-23 15:56:33 -0700

    Add ask-handbook HTTP function

--- api/ask-handbook/index.js
+++ api/ask-handbook/index.js
@@ -1,18 +1,12 @@
 module.exports = async function (context, req) {
-  const question =
-    req.query.q ||
-    (req.body && req.body.q) ||
-    "No question provided";
+  const question = req.query.q || "No question provided";
 
   context.res = {
     status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
+    headers: { "Content-Type": "application/json" },
     body: {
-      success: true,
-      received: question,
-      message: "ask-handbook API is working"
+      answer: `You asked: "${question}". (AI coming next)`
     }
   };
 };
+
